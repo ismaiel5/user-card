3f023693f4f2f824790dae98c175b3178ded938e
ismaiel <ismaiel.altahan@gmail.com>
2021-12-09 11:24:53 +0100

    - define an html template - use the shadow DOM - use the shadow root to append the html template

--- userCard.js
+++ userCard.js
@@ -1,8 +1,19 @@
+const template = document.createElement('template');
+template.innerHTML= `
+<div>
+<h3> Name </h3>
+</div>
+`;
+
+
 class UserCard extends HTMLElement{
     constructor(){
         super();
+
+        this.attachShadow({mode: 'open'});
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
     }
 }
-
 
 window.customElements.define('user-card', UserCard)
